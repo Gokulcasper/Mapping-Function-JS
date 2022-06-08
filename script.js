5f40bc0354c25ca0be3777f44f7f61c667d0cecc
Gokulcasper <ronitcasper215@gmail.com>
2022-06-08 08:25:58 +0530

    Feat : Adding index value

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const cart_item = {
   value1: { id: 1, itemName: "iphone", cost: "30000" },
-  value2: { id: 1, itemName: "microsoft mobile", cost: "6000" },
-  value3: { id: 1, itemName: "android mobile", cost: "10000" },
+  value2: { id: 2, itemName: "microsoft mobile", cost: "6000" },
+  value3: { id: 3, itemName: "android mobile", cost: "10000" },
 };
 
 // /-----------array iteration------------
@@ -13,5 +13,14 @@ const cart_item = {
 // -----------------object iteration----------
 
 let allPhones = Object.keys(cart_item).map((key) => {
-  console.log(cart_item[key].itemName);
+  return {
+    id: cart_item[key].id,
+    itemName: cart_item[key].itemName,
+    cost: cart_item[key].cost,
+  };
 });
+
+// console.log(allPhones);
+let indexOfKeys = Object.keys(cart_item);
+console.log("indexOfKeys :>> ", indexOfKeys);
+console.log(indexOfKeys.indexOf("value3"));
